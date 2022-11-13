9552a79426529676f645da63f2b5c85172f6bcdc
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-11-14 00:58:56 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// initialize const
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// put routers here
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+})
+module.exports = app;
