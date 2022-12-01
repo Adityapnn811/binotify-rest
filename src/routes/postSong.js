447757a920f0b91c5dc11a34397c2cfd3756c787
nart4hire <nart4hire@gmail.com>
2022-12-01 14:18:55 +0700

    Feat: Added C endpoint

---
+++ src/routes/postSong.js
@@ -0,0 +1,41 @@
+// Database Connection
+const prisma = require('../prisma')
+
+// Middleware
+const post = require("../utils/upload")
+
+module.exports = async (req, res) => {
+    try {
+        await post(req, res);
+
+        if (req.file == undefined) {
+            return res.status(400).send({
+                msg: "File not found/Wrong Filetype"
+            })
+        }
+
+        const query = await prisma.song.create({
+            data: {
+                Judul: req.body.judul,
+                Audio_path: '/song/' + req.file.filename,
+                Penyanyi: {
+                    connect: {
+                        user_id: req.userId
+                    }
+                }
+            }
+        })
+
+        res.status(200).send({
+            msg: `Uploaded`,
+            query
+        })
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({
+            msg: `Error uploading.`,
+            err: e
+        })
+    }
+}
